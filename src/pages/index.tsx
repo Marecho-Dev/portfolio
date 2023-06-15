4a3a7262e383319bed5792632121aaf8e1eb746d
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-06-15 00:28:18 -0400

    updated tailwind global values and header color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex h-14 items-center justify-end gap-10 bg-black pr-5 text-white">
+      <div className="flex h-14 items-center justify-end gap-10 bg-slate-900 pr-5 text-slate-400">
         <div>About</div>
         <div>Projects</div>
         <div>Contact</div>
