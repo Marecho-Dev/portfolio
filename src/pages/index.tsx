a3d4b930547bb17bf5e7e374d4cff91787371d46
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-06-15 00:38:22 -0400

    updated elements being used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,23 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex h-14 items-center justify-end gap-10 bg-slate-900 pr-5 text-slate-400">
-        <div>About</div>
-        <div>Projects</div>
-        <div>Contact</div>
-      </div>
+      <header className="flex h-16 items-center justify-end bg-slate-900 pr-5 text-slate-400">
+        <nav>
+          <ul className="flex gap-10">
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section>{/* About section */}</section>
+
+        <section>{/* Projects section */}</section>
+
+        <section>{/* Contact section */}</section>
+      </main>
     </>
   );
 };
